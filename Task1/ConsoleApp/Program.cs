fd495fbb3488dcfc2aa40a6355e2bd1d29e4d933
1TopG <ipz232_pov@student.ztu.edu.ua>
2025-01-25 18:02:23 +0200

    Created sum of two numbers console app

---
+++ Task1/ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{a} + {b} = {a+b}");
